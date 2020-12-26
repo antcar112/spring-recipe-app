02d3f7d6fd18eb0e84bca4d7d1f4970848b62010
Anthony Caron <antcar1992@gmail.com>
2020-12-26 11:27:29 -0800

    Added Notes and Recipe models

---
+++ src/main/java/com/anthonycaron/springrecipeapp/domain/Notes.java
@@ -0,0 +1,46 @@
+package com.anthonycaron.springrecipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob // Tells DB to expect strings longer then default 255 char limit
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
